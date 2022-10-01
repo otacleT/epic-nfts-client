ed8bbfc12f4e28fa3bdf46e5125d67a3bc647849
MIYABETaisei <mytspe46@gmail.com>
2022-10-01 11:25:59 +0900

    ✨ イベントを取得する関数

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { myEpicNftInterface } from "../hook/EpicNFT/EpicNFT";
 
 const Home: NextPage = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const CONTRACT_ADDRESS = "0x9212229e5c351bAe53cA31625033c525fe4F81bE";
   console.log("currentAccount: ", currentAccount);
 
   const checkWalletConnected = async () => {
@@ -21,6 +22,7 @@ const Home: NextPage = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+      setupEventListener();
     } else {
       console.log("No authorized account found");
     }
@@ -38,13 +40,39 @@ const Home: NextPage = () => {
       });
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+      setupEventListener();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNftInterface,
+          signer
+        );
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(
+            `あなたのウォレットにNFTを送信しました。OpenSeaに表示されるまでに最大で10分かかることがあります。NFTへのリンクはこちらです：https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+        });
+        console.log("Setup event listener!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0x866272dc86cd385b9db41c720587cf512e5067be";
     try {
       const { ethereum } = window;
       if (ethereum) {
